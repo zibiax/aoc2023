aedfb6c741ff2708635e02f573b836538538c7d0
Martin <martin.evenbom@gmail.com>
2025-12-01 16:30:34 +0100

    start on 2025

--- day1/day1.py
+++ day1/day1.py
@@ -1,71 +1,8 @@
 from timeit import default_timer as timer
 
-
-def input(filename):
-    with open(filename, 'r') as f:
-        input = f.read().splitlines()
-
-    return input
-
-def first(input):
-
-    total = 0
-    left = []
-    right = []
-
-    for line in input:
-        l1, r1 = map(int, line.split())
-
-        left.append(l1)
-        right.append(r1)
-
-    left.sort()
-    right.sort()
-
-    
-    while left and right:
-        l1 = left.pop(0)
-        r1 = right.pop(0)
-
-        total += abs(l1 - r1)
-    return total
+def read_input(input):
+    with open(f'{input}.txt', 'r') as f:
+        data = f.read().splitlines()
+    return data
 
 
-def second(input):
-
-    total = 0
-    left = []
-    right = []
-
-    for line in input:
-        l1, r1 = map(int, line.split())
-
-        left.append(l1)
-        right.append(r1)
-
-    left.sort()
-    right.sort()
-
-    for i in left:
-        times = 0
-
-        for x in right:
-            if i == x:
-                times += 1
-
-        score = i * times
-        total += score
-
-    return total
-    
-
-if __name__ == "__main__":
-    data = input('input.txt')
-    start = timer()
-    print(first(data))
-    end = timer()
-    print(end - start)
-    start = timer()
-    print(second(data))
-    end = timer()
-    print(end - start)
